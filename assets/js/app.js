0dc47aeaaee4cb365d959644e58d5c87c801b60a
your_name <prajakta77865@gmail.com>
2023-07-17 16:56:51 +0530

    b-4-layout

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+let cl = console.log;
+
+// const owl = document.querySelector('.owl-carousel')
+
+$('#tmSlider').owlCarousel({
+   
+        loop : true,
+        nav : true,     
+        navText : ['<i class="fa-solid fa-arrow-left"></i>', '<i class="fa-solid fa-arrow-right"></i>'],
+        autoplay : true,
+        margin : 20,
+        responsive : {
+            0 : {
+                items : 1,
+                dots: false,
+            },
+            600 : {
+                items : 3
+            },
+            1100 : {
+                items : 5,
+                margin: 20
+            }           
+        }  
+})
+// cl($('.tmSlider'))
